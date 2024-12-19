5eb68ab3eabf3e77b6a3ca2b1ff2beb709e042be
System Administrator <makovska.mariia@bws.pp.ua>
2024-12-19 10:24:16 +0200

    create db models and mock data for testing, add api package to Hotels.csproj

---
+++ Db.cs
@@ -0,0 +1,57 @@
+namespace HotelStore.DB;
+public record Room
+	{
+		public string ? roomType {get; set;} 
+		public int amount {get; set;}
+	}
+public record Hotel
+ 	{
+		public int id {get; set;} 
+		public string ? name { get; set; }
+		public string ? location { get; set; }
+		public double ? rating { get; set; }
+		public string ? imageUrl { get; set; }
+		public List<string> ? datesOfTravel { get; set; }
+		public string ? boardBasis { get; set; }
+		public List<Room> ? rooms { get; set; }
+	}
+
+public class HotelDB
+ {
+   private static List<Hotel> _hotels = new List<Hotel>()
+   {
+     new Hotel{ id=1, name="Seaside Paradise", location="Maldives", rating=4.9 },
+     new Hotel{ id=2, name="Cooperaor", location="Ukraine", rating=2.8 },
+   };
+
+   public static List<Hotel> GetHotels() 
+   {
+     return _hotels;
+   } 
+
+   public static Hotel ? GetHotel(int id) 
+   {
+     return _hotels.SingleOrDefault(hotel => hotel.id == id);
+   } 
+ }
+ 
+ // {
+ // "id": 1,
+ // "name": "Seaside Paradise",
+ // "location": "Maldives",
+ // "rating": 4.9,
+ // "imageUrl": "https://example.com/images/seaside-paradise.jpg",
+ // "datesOfTravel": ["2024-01-01", "2024-01-07"],
+ // "boardBasis": "All Inclusive",
+ // "rooms": [
+ // {
+ // "roomType": "Deluxe Suite",
+ // "amount": 5
+ // },
+ // {
+ // "roomType": "Family Room",
+ // "amount": 3
+ // }
+ // ]
+
+
--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
+using Microsoft.OpenApi.Models;
+using HotelStore.DB;
+
 var builder = WebApplication.CreateBuilder(args);
+    
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hotels API", Description = "Shows hotels info", Version = "v1" });
+});
+    
 var app = builder.Build();
-
+    
+if (app.Environment.IsDevelopment())
+{
+   app.UseSwagger();
+   app.UseSwaggerUI(c =>
+   {
+      c.SwaggerEndpoint("/swagger/v1/swagger.json", "Hotels API V1");
+   });
+}
+app.MapGet("/hotels", () => HotelDB.GetHotels());    
 app.MapGet("/", () => "Hello World!");
-
+    
 app.Run();
